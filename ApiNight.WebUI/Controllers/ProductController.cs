b7aefae6c5de3357e66598da33d17ea2a67d718f
gizemhazalakbulut <gizemhazalakbulut123@gmail.com>
2025-07-11 16:46:30 +0300

    3.ders yarısı

---
+++ ApiNight.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using ApiNight.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiNight.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> ProductList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7196/api/Products");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7196/api/Products", stringContent); // Api ye post isteği gönderiyoruz. PostAsync'ın içinde hem adres var hemde göndereceğimiz içerik
+            return RedirectToAction("ProductList");
+        }
+    }
+}
